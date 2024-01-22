9a8d10fddb24e9cb0cae2748a58748b087b8541d
Marvin <marvinraj77@gmail.com>
2024-01-23 01:07:11 +0800

    extract specific elements and attributes

--- main.py
+++ main.py
@@ -8,10 +8,13 @@ page = requests.get(URL)
 soup = BeautifulSoup(page.content, "html.parser")
 results = soup.find(id="ResultsContainer")
 
+# --- code below to create a html file and fill it with the content of the url ---
+content = page.text
+file_html = open("site.html", "w")
+file_html.write(content)
 
-# finds all jobs
+# --- finds all jobs (job title and company name) by class names and text content ---
 job_elements = results.find_all("div", class_="card-content")
-
 for job_element in job_elements: # prints out each job's title and company
     title_element = job_element.find("h2", class_="title")
     company_element = job_element.find("h3", class_="company")
@@ -19,9 +22,16 @@ for job_element in job_elements: # prints out each job's title and company
     print(company_element.text.strip())
     print()
 
+# --- finds all jobs (job title and company name) that are related to python ---
+python_jobs = results.find_all("h2", string=lambda text:"python" in text.lower())
+for job_element in python_jobs: 
+    title_element = job_element.parent.parent.parent.find("h2", class_="title")
+    company_element = job_element.parent.parent.parent.find("h3", class_="company")
+    print(title_element.text.strip())
+    print(company_element.text.strip())
+    print()
 
-# code below to create a html file and fill it with the content of the url
-# content = page.text
-# file_html = open("site.html", "w")
-# file_html.write(content)
-
+# --- find exact attributes from html ---
+for job_element in job_elements:
+    link_url = job_element.find_all("a")[1]["href"]
+    print(link_url)
